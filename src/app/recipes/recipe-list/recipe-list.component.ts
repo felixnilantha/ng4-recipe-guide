f89b1e1a9846bfb4f81447b9ff696e038744809c
Nilantha Silva <felixnilantha@gmail.com>
2017-07-05 00:26:13 +1000

    Added Recipe model and updated recipe list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from '../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+// Recipe is the recipe class created in the recipe model
+  recipes: Recipe[] = [
+    new Recipe('Recipe 1', 'dummy data', 'http://www.seriouseats.com/recipes/assets_c/2015/12/20151218-braised-chicken-thigh-cabbage-pancetta-recipe-kenji-14-thumb-1500xauto-428923.jpg'),
+    new Recipe('Recipe 2', 'dummy data', 'http://www.seriouseats.com/recipes/assets_c/2015/12/20151218-braised-chicken-thigh-cabbage-pancetta-recipe-kenji-14-thumb-1500xauto-428923.jpg')
+
+
+  ];
 
   constructor() { }
 
