badd975dd583c1dab746c528d3f5506646e8e3d2
Nilantha Silva <felixnilantha@gmail.com>
2017-07-07 13:14:07 +1000

    Added dropdown directive to implement the dropdown menu feature on Header and Recipe details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DropdownDirective } from './global/dropdown.directive';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +21,9 @@ import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/sh
     RecipeDetailsComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoppingListEditComponent
+    ShoppingListEditComponent,
+    DropdownDirective
+
   ],
   imports: [
     BrowserModule
